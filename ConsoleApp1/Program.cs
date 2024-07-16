3a50342529cdf8ffa5f695321f163132a8e5daab
rohat <rohat@ROHATGUNES>
2024-07-17 03:53:43 +1000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("hello rohat");
+            char karakter = 's';
+            //strıngler aslında karekterleden olulşan bir dizi//
+
+            Console.WriteLine("luıtfen adınızı gırıniz");
+            int yas2 =Convert .ToInt32(Console.ReadLine());
+            Console.WriteLine("kisinin yası ");
+
+
+
+
+
+        }
+
+
+
+    };
+
+
