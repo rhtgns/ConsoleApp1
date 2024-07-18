3b0f0b52a96c7785352e9f64bfd06ecf0700dc76
Rohat Güneş <rohatgns44@gmail.com>
2024-07-19 05:30:33 +1000

    home work 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,22 +6,12 @@ namespace helloWorld
     {
     static void Main(string[] args)
         {
-            Console.WriteLine("hello rohat");
-            char karakter = 's';
-            //strıngler aslında karekterleden olulşan bir dizi//
-
-            Console.WriteLine("luıtfen adınızı gırıniz");
-            int yas2 =Convert .ToInt32(Console.ReadLine());
-            Console.WriteLine("kisinin yası ");
-
-
-
-
-
+            Console.WriteLine("rohat gunes" +
+                "patika uzman kadrosu ve tecrübeli eğitmenleri sayeisnde yazilim dünyasına hızlı bir şekilde yapabileceğim iöin bootcampteyim ");
+            
         }
 
 
-
-    };
+    }
 
 
